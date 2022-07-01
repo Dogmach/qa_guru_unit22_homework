bb6591261ff4c8bc1f77cd19daa3b7a85134a5ee
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-07-01 15:41:52 +0400

    fixed

--- src/test/java/config/Credentials.java
+++ src/test/java/config/Credentials.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.ConfigFactory;
 
 public class Credentials {
 
-    public static CredentialsConfig config = ConfigFactory.create( CredentialsConfig.class);
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     public static boolean isBrowserStack() {
         return System.getProperty("device").equals("browserstack");
